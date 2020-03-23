3f97c3ef0de8aca5c23c3760f66479e42bd64eeb
Maxim Petischak <petrischak.maxim@gmail.com>
2020-03-23 21:20:26 +0200

    feat: create function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const pairs = [...str];
+    for (let i = 0; i < pairs.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (
+                pairs[i] === bracketsConfig[j][0] &&
+                pairs[i + 1] === bracketsConfig[j][1]
+            ) {
+                pairs.splice(i, 2);
+                i = -1;
+            }
+            if (pairs.length === 0) return true;
+        }
+    }
+    return false;
+};
